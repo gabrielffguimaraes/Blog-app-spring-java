f6d61023f1180e08567dbfec467339cdb1af3e9c
Gabriel Ferreira Guimarães <gabrielffguimaraes@gmail.com>
2021-08-25 23:44:30 -0300

    a

--- src/main/java/com/spring/project/blog/model/entity/Post.java
+++ src/main/java/com/spring/project/blog/model/entity/Post.java
@@ -14,5 +14,50 @@ public class Post {
     @Column(name = "id")
     private Long id;
 
+    @NotBlank
+    @Column(name = "titulo")
+    private String titulo;
+
+    @NotBlank
+    @Column(name = "autor")
+    private String autor;
+
+    @NotBlank
+    @Column(name = "texto")
+    private String texto;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
 
 }
